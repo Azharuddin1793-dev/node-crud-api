cdf9dc19904727f3fc2654e834cff6d5b481928d
Azhar Uddin <95132296+Azharuddin1793-dev@users.noreply.github.com>
2026-01-14 16:21:34 +0530

    Add welcome route to the serverchange

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", userRoutes);
+app.get("/", (req, res) => {
 
+return res.send("Welcome to my project");
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
